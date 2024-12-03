0e734543386359e721f2161c5d3b7e2e4fede63f
Diongie Fundador <121327561+JunzFundz@users.noreply.github.com>
2024-12-04 07:46:22 +0800

    new added

---
+++ script.js
@@ -0,0 +1,9 @@
+function navshow() {
+    let x = document.getElementById('show');
+
+    if(x.style.display==="block"){
+        x.style.display="none";
+    }else{
+        x.style.display="block";
+    }
+}
